34c807af44b239945d2e9a0f9ff0e0f63667edb2
drphamwit <44263198+drphamwit@users.noreply.github.com>
2022-03-29 22:02:39 -0400

    Initial commit
    The code template for Fizz Buzz game using Maven Java project

---
+++ src/main/java/com/demo/FizzBuzz.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class FizzBuzz 
+{
+    String processNumber(int num)
+	{	
+    	// Copy your code here
+		return "";
+	}
+	
+	String processNumbers (int[] numList)
+	{
+    	// Copy your code here
+		return "";
+	}
+
+}
---
+++ src/test/java/com/demo/FizzBuzzTest.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import junit.framework.TestCase;
+
+public class FizzBuzzTest extends TestCase {
+	
+	public void testProcessNumber() {
+		FizzBuzz fb = new FizzBuzz();
+		// Add your test cases here
+		assertEquals("1", fb.processNumber(1));
+	}
+
+	public void testProcessNumbers() {
+		FizzBuzz fb = new FizzBuzz();
+		//Add your test cases here
+	}
+}
