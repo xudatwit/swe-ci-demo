dbed435b3abc9b9cd99285a4d9bdfc872fdcd768
Duoduo Xu <56886703+xudatwit@users.noreply.github.com>
2022-03-30 14:45:53 -0400

    Update FizzBuzz.java

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -2,16 +2,34 @@ package com.demo;
 
 public class FizzBuzz 
 {
-    String processNumber(int num)
-	{	
-    	// Copy your code here
-		return "";
-	}
-	
-	String processNumbers (int[] numList)
+	String prcessNumber(int num)
 	{
-    	// Copy your code here
-		return "";
-	}
+    	if (num % 15 == 0) {
+            return "FizzBuzz";
+        }
+            else if (num % 3 == 0) {
+            return "Fizz";
+        }
+            else if (num % 5 == 0) {
+            return "Buzz";
+        } else {
+            return String.valueOf(num);
+            }
+    }
+    
+    String processNumbers (int[] numList)
+    {
+        // Copy your code here
+        String res = "";
+        for (int i = 0; i < numList.length; i++){
+            if (i != numList.length-1) {
+                res += processNumber(numList[i]) + ", ";
+                }
+            else {
+                res += processNumber(numList[i]);
+                }
+        }
+        return res;
+    }
 
 }
